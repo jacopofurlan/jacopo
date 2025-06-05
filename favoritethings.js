7c636531d19238ad6844a99f422e2da58ae2f5b0
jacopofurlan <jacopo.furlan99@gmail.com>
2025-06-05 23:54:17 +0200

    Add files via upload

--- favoritethings.js
+++ favoritethings.js
@@ -11,10 +11,16 @@ const parole = [
   { testo: "鍵", coloreTesto: "#8C9A98", sfondo: "#824444", font: "'Noto Serif JP', serif" },
   { testo: "DOCTOR WHO", coloreTesto: "#F0F0F0", sfondo: "#003B6F", font: "'Futura', serif" },
   { testo: 'Sonata op.111<br><span class="sottotitolo">L. v. Beethoven</span>', coloreTesto: "#001CD1", sfondo: "#A8DA1F", font: "'Crimson Text', serif" },
-  { testo: 'Liebestraum n° 3<br><span class="sottotitolo">F. Liszt</span>', coloreTesto: "#7416A7", sfondo: "#17C2D5", font: "'Noto Serif JP', serif", font: "'Crimson Text', serif" },
+  { testo: 'Liebestraum n° 3<br><span class="sottotitolo">F. Liszt</span>', coloreTesto: "#7416A7", sfondo: "#FFD043", font: "'Noto Serif JP', serif", font: "'Crimson Text', serif" },
   { testo: 'Abbey Road<br><span class="sottotitolo">The Beatles</span>', coloreTesto: "#FFF700", sfondo: "#20CCE2", font: "'Boldonse', system-ui" },
-  { testo: 'The Art of Fugue<br><span class="sottotitolo">J. S. Bach</span>', coloreTesto: "#165376", sfondo: "#90DAA5", font: "'Noto Serif JP', serif", font: "'Crimson Text', serif" }
-
+  { testo: 'The Art of Fugue<br><span class="sottotitolo">J. S. Bach</span>', coloreTesto: "#165376", sfondo: "#90DAA5", font: "'Noto Serif JP', serif", font: "'Crimson Text', serif" },
+  { testo: "Daft Punk", coloreTesto: "#EB5656", sfondo: "#000000", font: "'Synchro W01', sans-serif" },
+  { testo: 'Atom Heart Mother<br><span class="sottotitolo"> Pink Floyd</span>', coloreTesto: "#00F2FF", sfondo: "#86F055", font: "'Ewert', serif" },
+  { testo: "Peanuts", coloreTesto: "#000000", sfondo: "#FEDF1B", font: "'Ultra', serif" },
+  { testo: "Magritte", coloreTesto: "#9AE3E7", sfondo:"#000511", font: "'Sarina', cursive"},
+  { testo: "Мастер и Маргарита", coloreTesto: "#FFE45D", sfondo: "#FF0000", font: "'Metadannye', sans-serif"},
+  { testo: "Pietro Aretino", coloreTesto: "#C0C0C0", sfondo: "#8641CB", font: "'Snell Roundhand', serif" },
+  { testo: 'Lezioni Americane<br><span class="sottotitolo">Italo Calvino</span>', coloreTesto: "#2A91D6", sfondo:"#D8D6D8", font: "'Garamond', serif" } 
 ];
 
 const parolaSpeciale = {
@@ -25,7 +31,7 @@ const parolaSpeciale = {
 };
 
 // Opzione: true = parole in ordine, false = casuali
-const paroleInOrdine = false;
+const paroleInOrdine = true;
 
 let clickCounter = 0;
 let indiceParola = 0;
